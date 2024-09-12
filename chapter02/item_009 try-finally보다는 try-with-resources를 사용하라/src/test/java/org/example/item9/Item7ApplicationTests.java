ce44e1a4301a4c1428ed150cb685834167bff25e
Nhahan <kisy324@naver.com>
2024-09-12 19:02:01 +0900

    item9 - 2

---
+++ chapter02/item_009 try-finally보다는 try-with-resources를 사용하라/src/test/java/org/example/item9/Item7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.item9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
