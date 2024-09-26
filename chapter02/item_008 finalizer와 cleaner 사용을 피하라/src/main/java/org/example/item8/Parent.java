0e72ae6dd2d5c3fa5d6534113d6ee8dd16901ad4
Nhahan <kisy324@naver.com>
2024-09-26 15:55:17 +0900

    chapter2 chore

--- chapter02/item_008 finalizer와 cleaner 사용을 피하라/src/main/java/org/example/item8/Parent.java
+++ chapter02/item_008 finalizer와 cleaner 사용을 피하라/src/main/java/org/example/item8/Parent.java
@@ -4,4 +4,9 @@ public class Parent {
     Parent() {
         throw new RuntimeException("Parent 생성 불가");
     }
+
+    @Override
+    protected final void finalize() {
+        System.out.println("Parent 객체 소멸");
+    }
 }
